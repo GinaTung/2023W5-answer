11e298b6260a9082a9716811e6bbdf3bd179f7a8
yuling <gb2013429@gmail.com>
2023-08-12 10:58:35 +0800

    editor觸發

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,3 +1,5 @@
+import jQuery from 'jquery'
+window.$ = jQuery;
 ClassicEditor
   .create( document.querySelector( '#editor' ) )
   .then( editor => {
--- main.js
+++ main.js
@@ -1,5 +1,3 @@
 import './assets/scss/all.scss';
 console.log("Hello world!");
 
-import jQuery from 'jquery'
-window.$ = jQuery;
